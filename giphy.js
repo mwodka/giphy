1031f1a5f3af922bd77353bca1afd6b3560c4d1b
mwodka <mrw.simple@gmail.com>
2016-09-04 20:10:06 -0500

    working, but not pretty

--- giphy.js
+++ giphy.js
@@ -1,15 +1,18 @@
 var myArray = ['cat', 'dog', 'lizard', 'hamster', 'guinea pig', 'koala', 'bats', 'kanagroo', 'goat'];
 var giph = 'http://api.giphy.com/v1/gifs/search?limit=10&q=';
 var key = '&api_key=dc6zaTOxFJmzC';
-function makeButtons() {
 
+function makeButtons() {
+	
 	$('#animalbuttons').empty();
 
 	for (var i = 0; i < myArray.length; i++) {
 		var newButton = $('<button>');
+		newButton.attr('class', 'btn');
 		newButton.html(myArray[i]);
 		$('#animalbuttons').append(newButton);
 	}
+	createButtonListeners();
 }
 
 function getImages(img) {
@@ -20,8 +23,53 @@ function getImages(img) {
 			})
 			.done(function(response) {
 			var results = response.data;
+			createImages(results);
+			});
 }
 
-makeButtons();
+function createImages(obj) {
+	$('.animals').empty();
+	for (var i = 0; i < obj.length; i++) {
+		var newImage = $('<img>');
+		newImage.attr({
+			src: obj[i].images.fixed_width_still.url,
+			'class': 'gif',
+			'data-state': 'still',
+			'data-still': obj[i].images.fixed_width_still.url,
+			'data-animate': obj[i].images.fixed_width.url});
+		$('.animals').append(newImage);
+	}
+	createImageListeners();
+}
 
-// http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC   
+function changeState(img) {
+	if (img.dataset.state === 'still') {
+		img.dataset.state = 'animate';
+		$(img).attr('src', img.dataset.animate);
+	} else {
+		img.dataset.state = 'still';
+		$(img).attr('src', img.dataset.still);
+	}
+}
+
+function createImageListeners() {
+	$('.gif').on('click', function() {
+		changeState(this);
+	});
+}
+
+function createButtonListeners() {
+	$('.btn').on('click', function() {
+		getImages($(this).html());
+	});
+}
+
+$(document).ready(function() {
+	$('#add-animal').on('click', function() {
+		var input = $('#input').val().trim();
+		myArray.push(input);
+		makeButtons();
+	});
+});
+
+makeButtons();
